c4c1ff335a41911b44dbb9d793db38de4daafc5b
Dimitar Yankulov <dimitarqnkulow@gmail.com>
2025-02-09 20:35:33 +0200

    Add static superheroes

--- src/managers/superheroManager.js
+++ src/managers/superheroManager.js
@@ -1,6 +1,36 @@
 const uniqid = require("uniqid");
-const superheroes = [];
+const superheroes = [
+  {
+    id: "i78icswm6xoyg5n",
+    name: "Batman",
+    superPower: "Fight Skills",
+    imageUrl: "/images/BATMAN.svg",
+    humilityScore: 7,
+  },
+  {
+    id: "c8kichvkm6xoyg5n",
+    name: "Captain America",
+    superPower: "Shield Defence",
+    imageUrl: "/images/CAPTAIN-AMERICA.svg",
+    humilityScore: 8,
+  },
+  {
+    id: "c8kicswm6xopg5n",
+    name: "Superman",
+    superPower: "Flying",
+    imageUrl: "/images/SUPERMAN.svg",
+    humilityScore: 9,
+  },
+  {
+    id: "c8kicswm6xoyb7o",
+    name: "The Flash",
+    superPower: "Speed",
+    imageUrl: "/images/THE-FLASH.svg",
+    humilityScore: 6,
+  },
+];
 exports.getAll = () => superheroes.slice();
+exports.getOne = (superheroId) => superheroes.find((x) => x.id == superheroId);
 exports.create = (superheroData) => {
   const newSuperhero = {
     id: uniqid(),
