e16e0b242f8e37e85c3bf8ce501adb4805bc34fc
Dimitar Yankulov <dimitarqnkulow@gmail.com>
2025-02-09 15:40:12 +0200

    Add superhero to the list

--- src/controllers/superheroController.js
+++ src/controllers/superheroController.js
@@ -1,10 +1,18 @@
 const router = require("express").Router();
+const superheroManager = require("../managers/superheroManager");
 
 router.get("/create", (req, res) => {
+  console.log(superheroManager.getAll());
   res.render("create");
 });
 router.post("/create", (req, res) => {
-  console.log(req.body);
+  const { name, superPower, imageUrl, humilityScore } = req.body;
+  superheroManager.create({
+    name,
+    superPower,
+    imageUrl,
+    humilityScore: Number(humilityScore),
+  });
   res.redirect("/");
 });
 module.exports = router;
