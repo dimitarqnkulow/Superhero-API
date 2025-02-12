cc48f286ad3416416d1e78af80da7c1a91fbcf17
Dimitar Yankulov <dimitarqnkulow@gmail.com>
2025-02-12 09:56:09 +0200

    Add catching error for  superhero post request

--- src/controllers/superheroController.js
+++ src/controllers/superheroController.js
@@ -5,19 +5,28 @@ router.get("/create", (req, res) => {
   console.log(superheroManager.getAll());
   res.render("create");
 });
-router.post("/create", (req, res) => {
-  const { name, superPower, imageUrl, humilityScore } = req.body;
-  superheroManager.create({
-    name,
-    superPower,
-    imageUrl,
-    humilityScore: humilityScore,
-  });
-  res.sendStatus(200);
-  res.redirect("/");
-});
 router.get("/details/:superheroId", (req, res) => {
   const superhero = superheroManager.getOne(req.params.superheroId);
   res.render("details", { ...superhero });
+});
+router.post("/create", (req, res) => {
+  try {
+    const superheroData = req.body;
+    superheroManager.create({
+      name: superheroData.name,
+      superPower: superheroData.superPower,
+      imageUrl: superheroData.imageUrl,
+      humilityScore: Number(superheroData.humilityScore),
+    });
+    res.redirect("/");
+    res.sendStatus(200);
+    return res.json(superheroData);
+  } catch (error) {
+    console.log(error);
+
+    res.render("create", { error: error.message });
+    // res.sendStatus(400);
+    // res.json({ error: error.message });
+  }
 });
 module.exports = router;
